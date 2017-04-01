3b796c4ee9c943612221e109d5c52bcf816c86c6
amirensit <amirensit@gmail.com>
2017-04-01 14:32:59 +0200

    arkanoid go my code game

---
+++ Arkanoid/Assets/Scripts/Block.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour {
+
+    public ManagerCanvasScript manager;
+
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+
+        Destroy(gameObject);
+        manager.score += 10;
+
+    }
+
+
+}
---
+++ Arkanoid/Assets/Scripts/ManagerCanvasScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerCanvasScript : MonoBehaviour {
+
+    public float score;
+    public Text scoreText,GameOverText;
+    
+    public void Start()
+    {
+        score = 0;
+        GameOverText.text = "";
+    }
+    public void FixedUpdate()
+    {
+        scoreText.text = "" + score;
+    }
+}
